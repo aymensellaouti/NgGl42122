62d95faf8c2c1c5b223baeb53b038d3625b37b17
aymen2782 <aymen.sellaouti@gmail.com>
2021-12-02 18:58:01 +0100

    directive structurelle

--- src/app/cv/list/list.component.ts
+++ src/app/cv/list/list.component.ts
@@ -13,8 +13,8 @@ export class ListComponent implements OnInit {
     this.cvs = [
       new Cv(1, 'sellaouti', 'aymen', 'rotating_card_profile.png','teacher', '1234', 39),
       new Cv(2, 'sellaouti', 'aymen', 'rotating_card_profile2.png','teacher', '1234', 39),
-      // new Cv(3, 'sellaouti', 'aymen', '                 ','teacher', '1234', 39),
-      // new Cv(4, 'sellaouti', 'aymen', '','teacher', '1234', 39),
+      new Cv(3, 'sellaouti', 'aymen', '                 ','teacher', '1234', 39),
+      new Cv(4, 'sellaouti', 'aymen', '','teacher', '1234', 39),
     ];
   }
 
